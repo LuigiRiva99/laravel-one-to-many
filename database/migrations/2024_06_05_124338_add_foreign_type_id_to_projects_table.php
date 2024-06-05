a86a849b42b2753d221c1e388a6b63eecb0bfc11
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-06-05 15:05:00 +0200

    ADD: migration to add FK in project table and connection to types table

--- database/migrations/2024_06_05_124338_add_foreign_type_id_to_projects_table.php
+++ database/migrations/2024_06_05_124338_add_foreign_type_id_to_projects_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types');
         });
     }
 
@@ -22,7 +23,10 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+
+            $table->dropForeign('projects_type_id_foreign');
+
+            $table->dropcolumn('type_id');
         });
     }
 };
