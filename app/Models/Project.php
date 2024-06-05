e3831ed2d923eea33dc33a4af80e7b2df790bdd0
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-06-05 15:18:34 +0200

    ADD: Models one to many relation methods

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Project extends Model
 {
     use HasFactory;
@@ -12,4 +13,8 @@ class Project extends Model
     protected $fillable = [
         'title', 'description', 'link'
     ];
+
+    public function type() {
+        return $this->belongsTo(Type::class);
+    }
 }
